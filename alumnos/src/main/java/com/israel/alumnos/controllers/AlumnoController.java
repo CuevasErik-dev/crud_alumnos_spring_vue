6db3f4e981f663cd656f726b2688d4a313ec24db
CuevasErik-dev <cuevas.erik.israel@gmail.com>
2025-07-18 14:56:49 -0600

    task #Act-07-Creando metodo delete en el controlador

--- alumnos/src/main/java/com/israel/alumnos/controllers/AlumnoController.java
+++ alumnos/src/main/java/com/israel/alumnos/controllers/AlumnoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,11 @@ public class AlumnoController {
             Alumno actualizado = alumnoRepository.save(alumnoExistente);
             return ResponseEntity.ok(actualizado);
         }).orElse(ResponseEntity.notFound().build());
+    }
+
+    //metodo para eliminar un alumno 
+    @DeleteMapping("/eliminar-alumnos/{id}")
+    public void eliminarAlumno(@PathVariable long id){
+        alumnoRepository.deleteById(id);
     }
 }
