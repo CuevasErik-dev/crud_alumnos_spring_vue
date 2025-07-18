8b1462d00d78d15203d9bfc480509189485f2f0e
CuevasErik-dev <cuevas.erik.israel@gmail.com>
2025-07-18 14:44:36 -0600

    task #Act-06-Creando metodo Put en el contolador

--- alumnos/src/main/java/com/israel/alumnos/controllers/AlumnoController.java
+++ alumnos/src/main/java/com/israel/alumnos/controllers/AlumnoController.java
@@ -3,8 +3,11 @@ package com.israel.alumnos.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,21 +17,36 @@ import com.israel.alumnos.repository.AlumnoRepository;
 
 @RestController
 @RequestMapping("/alumnos")
-public class AlumnoController { 
+public class AlumnoController {
 
     @Autowired
     private AlumnoRepository alumnoRepository;
 
-    //metodo get para traer todos los alumnos de la base de Datos 
+    // metodo get para traer todos los alumnos de la base de Datos
     @GetMapping("/traer-alumnos")
     public List<Alumno> TraerAlumnos() {
         return alumnoRepository.findAll();
     }
 
-    //Metodo para Insertar un alumno a la base de datos
+    // Metodo para Insertar un alumno a la base de datos
     @PostMapping("/insertar-alumno")
-    public Alumno insertarAlumno(@RequestBody Alumno alumno){
+    public Alumno insertarAlumno(@RequestBody Alumno alumno) {
         return alumnoRepository.save(alumno);
     }
 
+    // metodo para editar un alumno
+    @PutMapping("/editar-alumnos/{id}")
+    public ResponseEntity<Alumno> actualizarAlumno(@PathVariable long id, @RequestBody Alumno alumno) {
+        return alumnoRepository.findById(id).map(alumnoExistente -> {
+            alumnoExistente.setNombre(alumno.getNombre());
+            alumnoExistente.setApellido(alumno.getApellido());
+            alumnoExistente.setGmail(alumno.getGmail());
+            alumnoExistente.setNumeroControl(alumno.getNumeroControl());
+            alumnoExistente.setTelefono(alumno.getTelefono());
+            alumnoExistente.setCarrera(alumno.getCarrera());
+            alumnoExistente.setImagenurl(alumno.getImagenurl());
+            Alumno actualizado = alumnoRepository.save(alumnoExistente);
+            return ResponseEntity.ok(actualizado);
+        }).orElse(ResponseEntity.notFound().build());
+    }
 }
